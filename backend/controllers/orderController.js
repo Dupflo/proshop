dbe50e394a14d9a81a2fd3b2289b01deb2dd8bbe
Florian <floriandupuis1996@gmail.com>
2021-02-01 20:59:56 +0100

    Show Orders on Profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,4 @@
+import { json } from 'express'
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
@@ -79,4 +80,12 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid }
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id })
+  res.json(orders)
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
