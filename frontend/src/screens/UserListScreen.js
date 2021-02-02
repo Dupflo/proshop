9ad965c78c568964dd3e8371e850f0e9e818c7ae
Florian <floriandupuis1996@gmail.com>
2021-02-02 08:08:10 +0100

    Admin User Delete

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button, useAccordionToggle } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listUsers } from '../actions/userActions'
+import { listUsers, deleteUser } from '../actions/userActions'
 
 const UserListScreen = ({ history }) => {
   const dispatch = useDispatch()
@@ -15,16 +15,19 @@ const UserListScreen = ({ history }) => {
   const userLogin = useSelector((state) => state.userList)
   const { userInfo } = userLogin
 
+  const userDelete = useSelector((state) => state.userDelete)
+  const { success: successDelete } = userDelete
+
   useEffect(() => {
-    if (userInfo && useAccordionToggle.isAdmin) {
-      dispatch(listUsers())
-    } else {
-      history.push('/login')
-    }
-  }, [dispatch, history])
+    // if (userInfo && userInfo.isAdmin) {
+    dispatch(listUsers())
+    // }
+  }, [dispatch, history, successDelete])
 
   const deleteHandler = (id) => {
-    console.log('delete')
+    if (window.confirm('Are you sure ?')) {
+      dispatch(deleteUser(id))
+    }
   }
 
   return (
